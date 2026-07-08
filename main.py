f009ee07d724024a75875f158c22f059846a7b51
toottyyo-alt <toottyyo@gmail.com>
2026-07-08 23:05:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import requests
 import os
 
-def send_telegram(msg):
-    token = os.environ["TELEGRAM_TOKEN"]
-    chat_id = os.environ["TELEGRAM_CHAT_ID"]
+token = os.environ["TELEGRAM_TOKEN"]
+chat_id = os.environ["TELEGRAM_CHAT_ID"]
 
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {
-        "chat_id": chat_id,
-        "text": msg
-    }
+url = f"https://api.telegram.org/bot{token}/sendMessage"
+data = {
+    "chat_id": chat_id,
+    "text": "✅ 텔레그램 테스트 성공"
+}
 
-    requests.post(url, data=data)
-
-if __name__ == "__main__":
-    send_telegram("✅ 텔레그램 테스트 성공")
+r = requests.post(url, data=data)
+print(r.status_code)
+print(r.text)
