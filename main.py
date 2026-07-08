ae43fd3b7a588d197034af5e5fd0c6781b4e42db
toottyyo-alt <toottyyo@gmail.com>
2026-07-08 22:58:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
 import os
-
-URL = "https://www.cbnu.ac.kr/www/selectBbsNttList.do?bbsNo=313&key=1850"
-
-def get_latest_notice():
-    res = requests.get(URL)
-    soup = BeautifulSoup(res.text, "html.parser")
-
-    title = soup.select_one("table tbody tr td.subject a")
-    if title:
-        return title.text.strip()
-    return None
 
 def send_telegram(msg):
     token = os.environ["TELEGRAM_TOKEN"]
@@ -26,6 +14,4 @@ def send_telegram(msg):
     requests.post(url, data=data)
 
 if __name__ == "__main__":
-    notice = get_latest_notice()
-    if notice:
-        send_telegram(f"📢 새 공지 확인:\n{notice}")
+    send_telegram("✅ 텔레그램 테스트 성공")
